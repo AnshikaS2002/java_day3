cc52866c05597d9f1bf45094784843ee263460e4
Anshika Semwal <semwal.anshi.10@gmail.com>
2023-11-22 12:42:23 +0530

    snake ladder game initialization

---
+++ SnakeLadder.java
@@ -0,0 +1,11 @@
+public class SnakeLadder {
+    void game(int pos) {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder game");
+        int pos = 0;
+        SnakeLadder sl = new SnakeLadder();
+        sl.game(pos);
+    }
+}
