ca27c19f080e8d8f12bd304eba8aa158631df716
Anshika Semwal <semwal.anshi.10@gmail.com>
2023-11-22 12:58:00 +0530

    dice rolled

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,11 +1,16 @@
+import java.util.Random;
+
 public class SnakeLadder {
-    void game(int pos) {
+    int diceRoll() {
+        Random random = new Random();
+        int num = random.nextInt(6) + 1;
+        return num;
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder game");
         int pos = 0;
         SnakeLadder sl = new SnakeLadder();
-        sl.game(pos);
+        sl.diceRoll();
     }
 }
