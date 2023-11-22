a49983249505b825311d88b5e3267516222485eb
Anshika Semwal <semwal.anshi.10@gmail.com>
2023-11-22 15:08:44 +0530

    updated code for two players

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -14,18 +14,20 @@ public class SnakeLadder {
         return option;
     }
 
-    void game() {
+    int totalTurns() {
         int pos = 0;
         SnakeLadder sl = new SnakeLadder();
         ArrayList<Integer> positions = new ArrayList<>();
         positions.add(pos);
         int diceRollCnt = 0;
+        int turnCnt = 0;
 
         while (pos != 100) {
             int diceNum = sl.diceRoll();
             int option = sl.optionGeneration();
             if (option == 1) {
                 pos += diceNum;
+                turnCnt--;
             } else if (option == 2) {
                 pos -= diceNum;
             }
@@ -35,13 +37,19 @@ public class SnakeLadder {
                 pos -= diceNum;
             positions.add(pos);
             diceRollCnt++;
+            turnCnt++;
         }
-        return;
+        return turnCnt;
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder game");
         SnakeLadder sl = new SnakeLadder();
-        sl.game();
+        int player1TurnCnt = sl.totalTurns();
+        int player2TurnCnt = sl.totalTurns();
+        if (player1TurnCnt <= player2TurnCnt) // assuming player1 started the game
+            System.out.println("player1 won");
+        else
+            System.out.println("player 2 won");
     }
 }
