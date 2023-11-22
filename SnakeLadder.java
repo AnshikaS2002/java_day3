ffd9fd746974c8d4183139f6bd1854dd02a3e97b
Anshika Semwal <semwal.anshi.10@gmail.com>
2023-11-22 13:24:05 +0530

    Game starts

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -13,10 +13,28 @@ public class SnakeLadder {
         return option;
     }
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Snake and Ladder game");
+    void game() {
         int pos = 0;
         SnakeLadder sl = new SnakeLadder();
-        sl.diceRoll();
+
+        while (pos < 100) {
+            int diceNum = sl.diceRoll();
+            int option = sl.optionGeneration();
+            if (option == 1) {
+                pos += diceNum;
+            } else if (option == 2) {
+                pos -= diceNum;
+            }
+            if (pos < 0) {
+                pos = 0;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder game");
+        SnakeLadder sl = new SnakeLadder();
+        sl.game();
     }
 }
